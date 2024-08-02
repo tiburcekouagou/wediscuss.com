c750ce3d90e88625f194afd44290b9b35dc2b904
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-08-02 12:45:34 +0200

    migrations groupes créées

--- database/migrations/2024_08_02_102247_create_groups_table.php
+++ database/migrations/2024_08_02_102247_create_groups_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -22,6 +32,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('group_user');
         Schema::dropIfExists('groups');
     }
 };
