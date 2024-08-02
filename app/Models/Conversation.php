aee2031776cb282eb654cab17ace5b0412ea5c18
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-08-02 16:22:10 +0200

    relations créées

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -18,4 +18,49 @@ class Conversation extends Model
         'user_id2',
         'last_message_id',
     ];
+    /**
+     * Définit une relation de type "appartient à" avec le modèle Message en tant que dernier message.
+     * 
+     * Cette méthode établit une relation "appartient à" (belongsTo) entre ce modèle et le modèle Message,
+     * permettant d'identifier le dernier message associé.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *    Instance de la relation qui permet d'accéder au dernier message associé.
+     */
+    public function lastMessage()
+    {
+        // Établit la relation avec le modèle Message pour récupérer le dernier message
+        return $this->belongsTo(Message::class);
+    }
+
+    /**
+     * Définit une relation de type "appartient à" avec le modèle User pour l'utilisateur 1.
+     * 
+     * Cette méthode établit une relation "appartient à" (belongsTo) entre ce modèle et le modèle User
+     * en utilisant la colonne 'user_id1' pour identifier le premier utilisateur associé.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *    Instance de la relation qui permet d'accéder au premier utilisateur.
+     */
+    public function user1()
+    {
+        // Établit la relation en utilisant la clé étrangère 'user_id1' pour le premier utilisateur
+        return $this->belongsTo(User::class, 'user_id1');
+    }
+
+    /**
+     * Définit une relation de type "appartient à" avec le modèle User pour l'utilisateur 2.
+     * 
+     * Cette méthode établit une relation "appartient à" (belongsTo) entre ce modèle et le modèle User
+     * en utilisant la colonne 'user_id2' pour identifier le deuxième utilisateur associé.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *    Instance de la relation qui permet d'accéder au deuxième utilisateur.
+     */
+    public function user2()
+    {
+        // Établit la relation en utilisant la clé étrangère 'user_id2' pour le deuxième utilisateur
+        return $this->belongsTo(User::class, 'user_id2');
+    }
+
 }
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -19,4 +19,51 @@ class Group extends Model
         'owner_id',
         'last_message_id',
     ];
+
+    /**
+     * Définit une relation plusieurs-à-plusieurs entre Group et User.
+     * 
+     * Cette méthode établit une relation "plusieurs-à-plusieurs" (belongsToMany) entre le modèle Group et le modèle User.
+     * Elle utilise une table pivot nommée 'group_user' pour associer les groupes à plusieurs utilisateurs.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     *    Instance de la relation qui permet d'interroger les utilisateurs associés au groupe.
+     */
+    public function users()
+    {
+        // Établit la relation en utilisant la table pivot 'group_user'
+        // return $this->belongsToMany(User::class, 'group_user', 'group_id', 'user_id');
+        return $this->belongsToMany(User::class, 'group_user');
+    }
+
+    /**
+     * Définit une relation un-à-plusieurs entre Group et Message.
+     * 
+     * Cette méthode établit une relation "un-à-plusieurs" (hasMany) entre le modèle Group et le modèle Message.
+     * Cela signifie qu'un groupe peut avoir plusieurs messages associés.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     *    Instance de la relation qui permet d'interroger les messages associés au groupe.
+     */
+    public function messages()
+    {
+        // Établit la relation où un groupe peut avoir plusieurs messages
+        return $this->hasMany(Message::class);
+    }
+
+    /**
+     * Définit une relation de type "appartient à" avec le modèle User pour le propriétaire du groupe.
+     * 
+     * Cette méthode établit une relation "appartient à" (belongsTo) entre ce modèle et le modèle User,
+     * identifiant l'utilisateur propriétaire du groupe.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *    Instance de la relation qui permet d'accéder au propriétaire du groupe.
+     */
+    public function owner()
+    {
+        // Établit la relation avec le modèle User pour identifier le propriétaire du groupe
+        return $this->belongsTo(User::class);
+    }
+
 }
