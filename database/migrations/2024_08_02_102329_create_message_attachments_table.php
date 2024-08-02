08500a40cdb1295d3109d83bb92dad9b1cbbed55
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-08-02 13:04:40 +0200

    migrations pièce jointes

--- database/migrations/2024_08_02_102329_create_message_attachments_table.php
+++ database/migrations/2024_08_02_102329_create_message_attachments_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('message_attachments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('message_id')->constrained('messages');
+            $table->string('name', 255); // x2dTiOdfserfeoijOiJiOfeio.jpg
+            $table->string('path', 1024); // /images/x2dTiOdfserfeoijOiJiOfeio.jpg
+            $table->string('mime', 255); // jpg, png, xsl
+            $table->integer('size'); // taille du fichier
             $table->timestamps();
         });
     }
