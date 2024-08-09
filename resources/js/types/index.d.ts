b6df2a388fa45f6b13e627345150e3df5a09b196
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-08-09 12:20:34 +0200

    création de resource

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -2,12 +2,12 @@ export interface User {
   id: number;
   name: string;
   email: string;
-  email_verified_at: string;
-  avatar: string;
+  avatar?: string;
   is_admin: boolean;
-  blocked_at: string;
   created_at: string;
   updated_at: string;
+  last_message?: string;
+  last_message_date?: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('online', function (User $user) {
-    return $user;
+    return $user ? new UserResource($user) : null;
 });
