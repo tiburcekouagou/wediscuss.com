9223bd1a165a221be4596a85f788e3f09fcc5a22
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-08-09 12:42:47 +0200

    récupération des users connectés

--- resources/js/Layouts/ChatLayout.vue
+++ resources/js/Layouts/ChatLayout.vue
@@ -2,29 +2,54 @@
     <div>
         <!-- Navbar Gauche -->
         Chat Layout
+        <pre>{{ onlineUsersObj }}</pre>
         <!-- Zone d'affichage des messages -->
     </div>
 </template>
 
 <script setup lang="ts">
+// imports
 import { User } from '@/types';
-import { onMounted } from 'vue';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 
-onMounted(() => {
+// Données partagées
+
+// Données réactives de gestion des conversations
+
+// Utilisateurs connectés
+const onlineUsersObj = ref<Record<string, User>>({})
+
+
+/**
+ * Fonction pour rejoindre le canal 'online' et récupérer les utilisateurs connectés
+ */
+function setupChannel() {
     window.Echo.join('online')
     .here((users: User[]) => {
-        console.log('here', users)
+        const usersObj = Object.fromEntries(users.map((user) => [user.id, user]))
+        onlineUsersObj.value = usersObj;
     })
     .joining((user: User) => {
-        console.log('joining', user)
+        onlineUsersObj.value[user.id] = user;
     })
     .leaving((user: User) => {
-        console.log('leaving', user)
+        delete onlineUsersObj.value[user.id]
     })
     .error((error: any) => {
-        console.error('error', error)
+        console.error('Echo error', error)
     })
+}
+
+/** Hook pour se connecter au canal */
+onMounted(() => {
+    setupChannel();
 })
+
+/** Nettoyer le composant avant le démontage du composant */
+onBeforeUnmount(() => {
+    window.Echo.leave('online')
+})
+
 </script>
 
 <style scoped></style>
