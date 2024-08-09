d25da05adc53425a9fd5ae4a8345b4917b3658d1
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-08-09 11:57:05 +0200

    canal de présence mis en place

--- resources/js/Layouts/ChatLayout.vue
+++ resources/js/Layouts/ChatLayout.vue
@@ -6,6 +6,25 @@
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User } from '@/types';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    window.Echo.join('online')
+    .here((users: User[]) => {
+        console.log('here', users)
+    })
+    .joining((user: User) => {
+        console.log('joining', user)
+    })
+    .leaving((user: User) => {
+        console.log('leaving', user)
+    })
+    .error((error: any) => {
+        console.error('error', error)
+    })
+})
+</script>
 
 <style scoped></style>
